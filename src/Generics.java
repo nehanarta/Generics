edc43ce468d85ecf02e5412d97935c6cec5c0d04
nehanarta <nehabaswanthrao@gmail.com>
2023-04-24 14:16:21 +0530

    Added:find three maximum float

--- src/Generics.java
+++ src/Generics.java
@@ -1,19 +1,19 @@
 public class Generics {
-        public void maxInt(int x, int y, int z) {
+        public void maxFloat(float x, float y, float z) {
             if (x > y && x > z) {
-                System.out.println("Maximum Integers is = " + x);
+                System.out.println("Maximum Float is = " + x);
             }
             if (y > x && y > z) {
-                System.out.println("Maximum Integers is = " + y);
+                System.out.println("Maximum Float is = " + y);
             }
             if (z > x && z > y) {
-                System.out.println("Maximum Integers is = " + z);
+                System.out.println("Maximum Float is = " + z);
             }
         }
         public static void main(String[] args) {
-            System.out.println("Maximum number");
-            Generics obj=new Generics();
-            obj.maxInt(60,30,50);
+            System.out.println("Maximum float");
+            Generics obj1=new Generics();
+            obj1.maxFloat(2.1f,3.3f,4.4f);
         }
 
     }
